6c0bc81f7d4a45ad6837d98700f0de6dd2cb1d46
Rolando Murillo <rolando@bandd.co>
2014-08-09 19:02:23 -0400

    Login and sign up pages.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,5 @@
+class UsersController < ApplicationController
+  def new
+  	render layout: "flatpage"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'sessions/new'
+
   resources :palettes, only: [:index, :show, :edit]
   resources :colors,   only: [:create, :destroy]
 

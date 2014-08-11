394112b49cc44db87716f5a4f1ff5ca83d4630be
Gustavo Beathyate <gustavo.bt@me.com>
2014-08-11 16:32:57 -0500

    Add users#show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :palettes, only: [:index, :show, :edit]
   resources :colors,   only: [:create, :destroy]
-  resources :users,    only: [:new, :create, :edit, :update]
+  resources :users,    only: [:new, :create, :show, :edit, :update]
 
   root to: "palettes#new"
 end

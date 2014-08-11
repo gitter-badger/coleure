c2be5111ba4b37c7ad1c7623f9b50bef66a787b2
Gustavo Beathyate <gustavo.bt@me.com>
2014-08-11 16:56:23 -0500

    Add login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  root to: "palettes#new"
+
+  get :login,  to: "sessions#new"
+  get :logout, to: "sessions#destroy"
+
+  resources :sessions, only: [:create]
   resources :palettes, only: [:index, :show, :edit]
   resources :colors,   only: [:create, :destroy]
   resources :users,    only: [:new, :create, :show, :edit, :update]
-
-  root to: "palettes#new"
 end

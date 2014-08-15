e3709570b46a4a0b2903ec1e10b5962a77e71ff8
Rolando Murillo <rolando@bandd.co>
2014-08-15 19:24:02 -0400

    Updated title for My Palettes page.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140815183115) do
+ActiveRecord::Schema.define(version: 20140815230510) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20140815183115) do
     t.datetime "updated_at"
     t.integer  "user_id"
     t.integer  "parent_id"
+    t.integer  "children_count"
   end
 
   create_table "users", force: true do |t|

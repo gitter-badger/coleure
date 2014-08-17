5c2131c3223a108cd4cc50a5814fcdf334cccaed
Rolando Murillo <rolando@bandd.co>
2014-08-17 14:32:06 -0400

    Initial version of shared view.

--- app/assets/javascripts/coleure.js
+++ app/assets/javascripts/coleure.js
@@ -2,24 +2,20 @@
   window.addEventListener('load', function() {
     return require(['./goodies', './settings', './colors', './inspector', './palette', './clipboard', './formats'], function(_, settings, colors, inspector, palette) {
       "use strict";
-      colors.load({
-        colors: '/assets/colors.json',
-        template: '/assets/templates/color.html'
-      });
-      inspector.setup({
-        previewTemplate: '/assets/templates/preview.html',
-        singleTemplate: '/assets/templates/test/single.html',
-        doubleTemplate: '/assets/templates/test/double.html'
-      });
-      palette.setup({
-        template: '/assets/templates/color.html'
-      });
-      if(settings.upgradeNotice == undefined){ settings.upgradeNotice = true }
-      _.listen(_.id('upgradeNoticeButton'), 'mousedown', function(){
-        settings.upgradeNotice = false;
-        _.hide(_.id('upgradeNotice'))
-      });
-      if (settings.upgradeNotice == true) { _.show(_.id('upgradeNotice')) };
+      if(!_.id('shared_palette_colors')){
+        colors.load({
+          colors: '/assets/colors.json',
+          template: '/assets/templates/color.html'
+        });
+        inspector.setup({
+          previewTemplate: '/assets/templates/preview.html',
+          singleTemplate: '/assets/templates/test/single.html',
+          doubleTemplate: '/assets/templates/test/double.html'
+        });
+        palette.setup({
+          template: '/assets/templates/color.html'
+        });
+      }
     });
   });
 
